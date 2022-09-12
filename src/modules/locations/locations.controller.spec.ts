a8675a520bb18cef6d8602273bc4cd416338d41c
VIIGstar <trung.nb@hesperiafreepress.com>
2022-09-12 19:06:14 +0700

    draft save test controller create location

--- src/modules/locations/locations.controller.spec.ts
+++ src/modules/locations/locations.controller.spec.ts
@@ -3,15 +3,22 @@ import { LocationsController } from './locations.controller';
 import { LocationsService } from './locations.service';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Location } from './entities/location.entity';
-import { NotFoundException } from '@nestjs/common';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
 import { UserType } from '../users/users.constants';
 import { User } from '../users/entities/user.entity';
-import { LocationStatus } from './locations.contants';
+import {
+  DefaultSafeZoneRadius,
+  LocationNFTStatus,
+  LocationStatus,
+  LocationType,
+} from './locations.contants';
 import { LocationHandleService } from '../location-handle/location-handle.service';
 import { LocationHandle } from '../location-handle/entities/location-handle.entity';
+import { FindOptionsWhere, LessThanOrEqual, MoreThanOrEqual } from 'typeorm';
+import { CreateLocationDto } from './dto/create-location.dto';
 
 describe('LocationsController', () => {
-  it('user admin, allow blacklist, status <> approved', async () => {
+  it('FindOneLocation: user admin, allow blacklist, status <> approved', async () => {
     const model = new Location();
     model.is_blacklist = true;
     model.status = LocationStatus.PENDING;
@@ -26,7 +33,7 @@ describe('LocationsController', () => {
     ).resolves.toEqual(model);
   });
 
-  it('not found, location is blacklist but user not admin', async () => {
+  it('FindOneLocation: not found, location is blacklist but user not admin', async () => {
     const model = new Location();
     model.is_blacklist = true;
     const repoMock: jest.Mock<{ findOneBy: jest.Mock<Location, []> }, []> =
@@ -42,7 +49,7 @@ describe('LocationsController', () => {
     ).rejects.toEqual(new NotFoundException());
   });
 
-  it('not found, location is pending and owner is not correct', async () => {
+  it('FindOneLocation: not found, location is pending and owner is not correct', async () => {
     const model = new Location();
     model.status = LocationStatus.PENDING;
     model.user_id = 2;
@@ -64,6 +71,69 @@ describe('LocationsController', () => {
         model,
       ),
     ]);
+  });
+
+  it('CreateLocation: distance is less than 200m, status init with PENDING', async () => {
+    const model = new CreateLocationDto();
+    model.name = 'Near vincom Tran Duy Hung';
+    model.lat = 21.007650506937715;
+    model.long = 105.79656486631434;
+    model.map_captured = 'https://nftdiadiem.com/vincom-tran-duy-hung';
+    const mockRepo = jest.fn(() => ({
+      findBy: (option: FindOptionsWhere<Location>) => {
+        if (option.status !== LocationStatus.APPROVED) {
+          throw new BadRequestException();
+        }
+
+        const isObjDiff = (obj1, obj2) => {
+          return JSON.stringify(obj1) !== JSON.stringify(obj2);
+        };
+        switch (true) {
+          case isObjDiff(
+            option.safe_zone_bot,
+            LessThanOrEqual(21.009001310665933),
+          ):
+          case isObjDiff(
+            option.safe_zone_top,
+            MoreThanOrEqual(21.006299703209496),
+          ):
+          case isObjDiff(
+            option.safe_zone_left,
+            LessThanOrEqual(105.79791567004256),
+          ):
+          case isObjDiff(
+            option.safe_zone_right,
+            MoreThanOrEqual(105.79521406258613),
+          ):
+            throw new BadRequestException();
+        }
+        const loc = new Location();
+        loc.long = 105.79521595313413;
+        loc.lat = 21.00630297282199;
+        loc.block_radius = 50;
+
+        return [loc];
+      },
+    }));
+    const controller = await getControllerWithMockUserBlacklist(mockRepo);
+
+    const response = new Location();
+    Object.assign(response, model);
+    response.name = model.name;
+    response.lat = model.lat;
+    response.long = model.long;
+    response.map_captured = model.map_captured;
+    response.nft_status = LocationNFTStatus.PENDING;
+    response.type = LocationType.CUSTOMER;
+    response.block_radius = DefaultSafeZoneRadius;
+    response.country = 'VN';
+    response.status = LocationStatus.PENDING;
+    //sys
+    response.user_id = 1;
+    response.created_by_id = 1;
+    await expect(controller.create(model, { id: 1 } as User)).resolves.toEqual(
+      response,
+    );
   });
 });
 
