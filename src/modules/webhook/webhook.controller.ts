01087d6416cd391d9dc60eb5c3f4039d8f1b949e
VIIGstar <trung.nb@hesperiafreepress.com>
2022-09-21 11:01:24 +0700

    update rabbitmq

--- src/modules/webhook/webhook.controller.ts
+++ src/modules/webhook/webhook.controller.ts
@@ -1,12 +1,12 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 import { WebhookService } from './webhook.service';
 
 @Controller('webhook')
 export class WebhookController {
   constructor(private readonly webhookService: WebhookService) {}
 
-  @Post()
-  create(@Body() obj: any): void {
-    console.log(obj);
+  @Get('/vnpay')
+  webhookVNPay(@Req() req): void {
+    console.log('whoami? ', req.query);
   }
 }
