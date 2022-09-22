0f7d309457f6838523efc9085db83d029ee1747d
VIIGstar <trung.nb@hesperiafreepress.com>
2022-09-22 13:47:24 +0700

    preload relation to order

--- src/modules/user_package/user_package.service.spec.ts
+++ src/modules/user_package/user_package.service.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserPackageService } from './user_package.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Package } from '../package/entities/package.entity';
+import { UserPackage } from './entities/user_package.entity';
 
 describe('UserPackageService', () => {
   let service: UserPackageService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserPackageService],
+      providers: [
+        UserPackageService,
+        {
+          provide: getRepositoryToken(UserPackage),
+          useFactory: jest.fn(() => ({})),
+        },
+      ],
     }).compile();
 
     service = module.get<UserPackageService>(UserPackageService);
--- src/modules/user_package/user_package.service.ts
+++ src/modules/user_package/user_package.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserPackageDto } from './dto/create-user_package.dto';
 import { UpdateUserPackageDto } from './dto/update-user_package.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { UserPackage } from './entities/user_package.entity';
 
 @Injectable()
 export class UserPackageService {
+  constructor(
+    @InjectRepository(UserPackage)
+    private readonly repository: Repository<UserPackage>,
+  ) {}
+
   create(createUserPackageDto: CreateUserPackageDto) {
     return 'This action adds a new userPackage';
   }
