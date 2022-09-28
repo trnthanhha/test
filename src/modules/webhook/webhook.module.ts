47f95f965259604d185409e2cad0b47ebc3c751e
trungnb-viigstar <trungnb200149@gmail.com>
2022-09-28 19:41:42 +0700

    fix payment status IPN

--- src/modules/webhook/webhook.module.ts
+++ src/modules/webhook/webhook.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import WebhookController from './webhook.controller';
 import { WebhookFactory } from '../../services/message-broker/webhook.factory';
 import { RabbitMQServices } from '../../services/message-broker/webhook.types';
+import { BillsService } from '../bills/bills.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Bill } from '../bills/entities/bill.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Bill])],
   controllers: [WebhookController],
-  providers: [WebhookFactory.Build(RabbitMQServices.VNPay)],
+  providers: [WebhookFactory.Build(RabbitMQServices.VNPay), BillsService],
 })
 export class WebhookModule {}
