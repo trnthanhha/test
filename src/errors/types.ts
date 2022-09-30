547ab6dbe09990cc560ab84ea266ae881768692a
VIIGstar <trung.nb@hesperiafreepress.com>
2022-09-30 16:47:34 +0700

    purchase fail response user_package_id

--- src/errors/types.ts
+++ src/errors/types.ts
@@ -10,7 +10,7 @@ export class PrepareError extends Error {
 export class SkipError extends Error {
   constructor(ex: any, msg: string) {
     super(ex);
-    this.stack = ex.stack;
+    this.stack = ex?.stack;
     this.message = msg;
     this.name = 'SkipError';
   }
