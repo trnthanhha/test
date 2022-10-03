b5220031fdbcdcb761d3641b4c6a1c007421ef1b
VIIGstar <trung.nb@hesperiafreepress.com>
2022-10-03 15:38:56 +0700

    get orders type = point

--- src/modules/locations/locations.controller.ts
+++ src/modules/locations/locations.controller.ts
@@ -29,7 +29,7 @@ import { UserType } from '../users/users.constants';
 import { Auth } from '../../decorators/roles.decorator';
 import { CreateLocationDto } from './dto/create-location.dto';
 import { LocationHandleService } from '../location-handle/location-handle.service';
-import { FindOptionsWhere, IsNull, Like, Repository } from 'typeorm';
+import { FindOptionsWhere, IsNull, Like, MoreThan, Repository } from 'typeorm';
 import { ValidateDistanceDto } from './dto/validate-distance-dto';
 import { UpdateLocationDto } from './dto/update-location.dto';
 import { UsersService } from '../users/users.service';
@@ -115,6 +115,13 @@ export class LocationsController {
       whereCondition.status = status as LocationStatus;
       if (user.type === UserType.CUSTOMER) {
         whereCondition.user_id = user.id;
+      }
+    }
+    if (owned && user) {
+      if (user.type === UserType.CUSTOMER) {
+        whereCondition.user_id = user.id;
+      } else {
+        whereCondition.user_id = MoreThan(0);
       }
     }
 
